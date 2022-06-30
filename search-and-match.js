ad1b732bd43ba902400cf1a313220de5a7c7f507
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-06-30 10:51:23 +0600

    second commit

--- search-and-match.js
+++ search-and-match.js
@@ -1,3 +1,7 @@
+// Commenting on your code is one of the best ways than taking physical notes on pen and paper; I do both ;
+
+// An array of JavaScript Books; Make a new array with the books which contains the exact string "JavaScript" in int. Have to be case-insensitive;
+
 const jsBooks = [
   "A Smarter Way to Learn JavaScript",
   "JavaScript: The Definitive Guide",
@@ -11,10 +15,13 @@ console.log("Best books on JavaScripts  = ", jsBooks);
 
 const search = "JAVASCRIPT";
 
+// if an empty array is declared for holding multiple elements later , then it must be declared with "let"; declaring with "const" will give errors;
 let matched = [];
 
 for (const book of jsBooks) {
-  //   console.log(book);
+  // .includes checks if the string exists or not;
+  // parenthesis () is a must for toLowerCase() and toUpperCase();
+  //.push() inserts new elements to an array;
   if (book.toLowerCase().includes(search.toLowerCase()) == true) {
     matched.push(book);
   }
